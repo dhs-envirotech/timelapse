c704a27f923aa6e07372335c35a5e8908ad2084c
humanfriend22 <humanfriend22@gmail.com>
2023-11-07 17:22:00 -0800

    add previous video

--- src/scripts/video.py
+++ src/scripts/video.py
@@ -24,13 +24,13 @@ fourcc = cv2.VideoWriter_fourcc(*'VP80')
 output_video = cv2.VideoWriter(timelapse_file, fourcc, frame_rate, (width, height))
 
 # Write!
-# if os.path.isfile(old_timelapse_mp4):
-#     old_timelapse_video = cv2.VideoCapture(old_timelapse_mp4)
-#     while old_timelapse_video.isOpened():
-#         r, frame = old_timelapse_video.read()
-#         if not r:
-#             break
-#         output_video.write(frame)
+if os.path.isfile(old_timelapse_file):
+    old_timelapse_video = cv2.VideoCapture(old_timelapse_file)
+    while old_timelapse_video.isOpened():
+        r, frame = old_timelapse_video.read()
+        if not r:
+            break
+        output_video.write(frame)
 
 for image_file in image_files:
     frame = cv2.imread(image_file)
