78ae31044f9f3e1bcff91f487e0f9a6474cae828
MrKaehms <kaehmsrobert@dublinusd.org>
2024-01-03 09:47:01 -0800

    updating video.sh - incremental updates

--- src/scripts/video.sh
+++ src/scripts/video.sh
@@ -1,7 +1,10 @@
 #/bin/bash
 
+#echo $1
+#exit
+
 # Remove Preview Image
-sudo rm /home/pi/timelapse/media/pictures/preview.jpg
+#sudo rm /home/pi/timelapse/media/pictures/preview.jpg
 
 # Run Python Script
 # sudo python3 /home/pi/timelapse/scripts/video.py
@@ -9,12 +12,12 @@ sudo rm /home/pi/timelapse/media/pictures/preview.jpg
 pictures="/home/pi/timelapse/media/pictures"
 videos="/home/pi/timelapse/media/videos"
 
-cd videos
+cd $videos
 mv timelapse.mp4 old-timelapse.mp4
 
-cd pictures
+cd $pictures
 ffmpeg -hide_banner -loglevel error -pattern_type glob -r 3 -i "*.jpg" -s 820x616 -vcodec libx264 "$videos/timelapse.mp4"
 # TODO: Archive images
 
-cd videos
+#cd videos
 # TODO: combine videos
