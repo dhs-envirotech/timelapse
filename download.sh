0559388b90678b2a8448f036e7a569b3a945be38
humanfriend22 <humanfriend22@gmail.com>
2024-01-03 23:35:28 -0800

    fit image to modal & check for git in download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Skip this for manual walk-through
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Run 'sudo apt install git'"
+    exit 1
+fi
+
 cd /home/pi
 git clone https://github.com/dhs-envirotech/timelapse
 mv timelapse temp
 
 mv temp/src timelapse
 mv temp/timelapse.py timelapse.py
+rm -rf temp
 
 echo "Download finished"
