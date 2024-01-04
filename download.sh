1558c583ba03cf5795db958f0ed580655535cddd
humanfriend22 <humanfriend22@gmail.com>
2024-01-03 22:33:09 -0800

    update download script

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ cd /home/pi
 git clone https://github.com/dhs-envirotech/timelapse
 mv timelapse temp
 
-mv temp/timelapse timelapse
+mv temp/src timelapse
 mv temp/timelapse.py timelapse.py
 
 echo "Download finished"
