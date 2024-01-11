81f401f7fb0e1f19b59b3ad92ff5642ef662cdf2
humanfriend22 <humanfriend22@gmail.com>
2024-01-10 19:26:21 -0800

    push archive.sh

--- timelapse.py
+++ timelapse.py
@@ -12,11 +12,14 @@ try:
 except subprocess.CalledProcessError:
     pass
 
-# Check for reset
-if len(sys.argv) == 1:
+# Action if chain
+try:
+    action = sys.argv[1]
+except NameError:
     print("Please provide action: 'setup' or 'reset'")
     exit(1)
-elif sys.argv[1] == 'reset':
+
+if action == 'reset':
     confirmation = input("Are you sure you want to erase all media? (type 'delete')\n> ")
 
     if confirmation != 'delete':
@@ -34,39 +37,35 @@ elif sys.argv[1] == 'reset':
     os.system(f'sudo rm -rf {media}')
     os.system(f'mkdir {media}')
 
-    logging.info('Finished reset media')
-    exit(0)
-elif sys.argv[1] != 'setup':
-    print("Please provide action: 'setup' or 'reset'")
-    exit(1)
+    logging.info('Finished reset media')    
+elif action == 'setup':
+    # Install project dependencies
+    os.system('sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install imagemagick python3-opencv python3-flask ffmpeg')
 
-# Install project dependencies
-os.system('sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install imagemagick python3-opencv python3-flask ffmpeg')
+    projectDirectory="/home/pi/timelapse"
 
-projectDirectory="/home/pi/timelapse"
+    # Legacy installs
+    if not os.path.exists(projectDirectory):
+        if os.path.exists("timelapse.tar.gz"):
+            # Just in case if the software is using the build.sh script still
+            os.system("tar -xzf timelapse.tar.gz timelapse")
+        # This else statement is not needed as it may catch a git clone install
+        # else:
+        #     print('Please transfer timelapse.tar.gz to this computer')
+        #     exit(1)
 
-# Legacy installs
-if not os.path.exists(projectDirectory):
-    if os.path.exists("timelapse.tar.gz"):
-        # Just in case if the software is using the build.sh script still
-        os.system("tar -xzf timelapse.tar.gz timelapse")
-    # This else statement is not needed as it may catch a git clone install
-    # else:
-    #     print('Please transfer timelapse.tar.gz to this computer')
-    #     exit(1)
+    # Write cron jobs
+    jobs = [
+        f"@reboot pi bash {projectDirectory}/scripts/server.sh",
+        # Every 3 hours, minute 0
+        f"0 */3 * * * pi bash {projectDirectory}/scripts/picture.sh",
+        # At minute 2, midnight
+        f"2 0 * * * pi bash {projectDirectory}/scripts/video.sh",
+        # At minute 5, on sunday "morning" (saturday night)
+        f"5 0 * * 0 pi bash {projectDirectory}/scripts/archive.sh"
+    ]
 
-# Write cron jobs
-jobs = [
-    f"@reboot pi bash {projectDirectory}/scripts/server.sh",
-    # Every 3 hours, minute 0
-    f"0 */3 * * * pi bash {projectDirectory}/scripts/picture.sh",
-    # At minute 2, midnight
-    f"2 0 * * * pi bash {projectDirectory}/scripts/video.sh",
-    # At minute 5, on sunday "morning" (saturday night)
-    f"5 0 * * 0 pi bash {projectDirectory}/scripts/archive.sh"
-]
+    with open("/etc/cron.d/timelapse", "w") as file:
+        file.write('\n'.join(jobs) + '\n')
 
-with open("/etc/cron.d/timelapse", "w") as file:
-    file.write('\n'.join(jobs) + '\n')
-
-os.system(f"sudo chmod +x {projectDirectory}/scripts/picture.sh {projectDirectory}/scripts/video.sh")
+    os.system(f"sudo chmod +x {projectDirectory}/scripts/picture.sh {projectDirectory}/scripts/video.sh")
